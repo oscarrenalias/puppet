155c6b9ac35a2f8daa8d8048e0e66dfdf6993357
Oscar Renalias <oscar@renalias.net>
2013-02-02 00:42:04 +0200

    Initial version

---
+++ base/manifests/init.pp
@@ -0,0 +1,57 @@
+#
+# Colllection of things that I always install
+#
+class base {
+  package { "git":
+    ensure => "present",
+  }	
+  
+  # this is not available in OS X
+  case $operatingsystem {
+    /(Ubuntu|Debian|Centos|RedHat)/: {
+      package { "emacs-nox":
+	ensure => "present",
+      }
+    }
+  }
+  
+  case $operatingsystem {
+    /(CentOS|RedHat)/: { include base::redhat }
+    "Ubuntu": { include base::ubuntu }
+    "Darwin": { include base::darwin }
+    default: { fail("Unsupported OS: $operatingsystem") }
+  }        
+}
+
+class base::ubuntu {
+  exec{ "apt-get-update":
+    command => "apt-get update",
+    path => [ "/usr/bin/" ],
+    before => Package["language-pack-id"],
+  }
+  
+  package { "language-pack-id":
+    ensure => "present",
+  }
+  
+  exec { "dpkg-reconfigure-locales":
+    command => "dpkg-reconfigure locales",
+    path => [ "/usr/sbin", "/usr/bin", "/bin", "/sbin" ],
+    logoutput => on_failure,
+    require => Package["language-pack-id"],
+  }
+}
+
+#
+# Centos/RHEL specific configuration
+#
+class base::redhat {
+  
+}
+
+#
+# OS X specific configuration
+#
+class base::darwin {
+
+}
