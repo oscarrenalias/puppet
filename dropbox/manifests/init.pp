aa608aa724231b13f7cc60062f23192021fee50d
Oscar Renalias <oscar@renalias.net>
2013-02-02 14:59:40 +0200

    Module for installing dropbox (currently only ubuntu 12.10 is supported)

---
+++ dropbox/manifests/init.pp
@@ -0,0 +1,31 @@
+class dropbox {
+  case $operatingsystem {
+    /(Ubuntu|Debian)/: { include dropbox::ubuntu }
+    default: { fail("Operating system $operatingsystem not supported yet") }
+  }
+}
+
+class dropbox::ubuntu inherits dropbox {
+  Exec { path => ["/bin", "/usr/bin"], logoutput => on_failure }
+  
+  exec { "add-dropbox-apt-key":
+    command => "apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E",
+    unless => "apt-key list | grep -i Dropbox",
+  }
+
+  exec { "add-dropbox-apt-repo":
+    # Even in 12.10 we have to use precise (12.04) instead of quatzal
+    command => "add-apt-repository \"deb http://linux.dropbox.com/ubuntu precise main\"",
+    unless => "grep dropbox /etc/apt/sources.list",
+  }
+
+  exec { "apt-get-update":
+    command => "apt-get update",
+    require => [Exec["add-dropbox-apt-key"], Exec["add-dropbox-apt-repo"]]
+  }
+
+  package { "nautilus-dropbox":
+    ensure => "present",
+    require => Exec["apt-get-update"],
+  }
+}  
