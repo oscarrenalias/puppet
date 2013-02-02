3ca5f19569950ae16c439e118cc6b1499a1ec9b4
Oscar Renalias <oscar@renalias.net>
2013-02-02 10:59:25 +0200

    Added code for installing the IcedTea browser plugin in Ubuntu

--- java/manifests/init.pp
+++ java/manifests/init.pp
@@ -1,16 +1,28 @@
 class java(
   $version = "7",
   $provider = "openjdk",
-  $type = "jdk"
+  $type = "jdk",
+  $enable_browser_plugin = true,
 ) {
 
-  $package = $operatingsystem ? {
-    /(Ubuntu|Debian)/ => "$provider-$version-$type",
-    default => fail("Operating system $operatingsystem not supported yet")
+  case $operatingsystem {
+    /(Ubuntu|Debian)/: {
+      $package = "$provider-$version-$type"
+      $browser_plugin_package = "icedtea-$version-plugin"
+    }
+    default: { fail("Operating system $operatingsystem not supported yet") }
   }
-  
+
   package { "java":
     name => $package,
     ensure => "present",
+  }
+
+  if($enable_browser_plugin) {
+    package { "java-browser-plugin":
+      name => $browser_plugin_package,
+      ensure => "present",
+      require => Package["java"],
+    }
   }
 }
