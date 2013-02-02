6be379d8d92b2b0d679d56310d265a26ca4a3b3b
Oscar Renalias <oscar@renalias.net>
2013-02-02 14:59:59 +0200

    Main manifest to be run when installing packages

---
+++ install.pp
@@ -0,0 +1,11 @@
+#
+# Comment/uncomment the needed modules and then run:
+#
+# puppet apply --moduledir=`pwd` apply install.pp
+#
+
+require base
+require chrome
+require encfs
+require dropbox
+require java
