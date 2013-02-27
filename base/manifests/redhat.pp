4e6408936c1580e15b9a092f38e82eeb9d5734e0
Oscar Renalias <oscar@renalias.net>
2013-02-27 23:29:59 +0200

    Added automatic configuration of EPEL for RHEL, CentOS and Fedora

--- base/manifests/redhat.pp
+++ base/manifests/redhat.pp
@@ -4,3 +4,14 @@
 class base::redhat inherits base {
   
 }
+
+#
+# Enables the EPEL repository for RPM systems
+#
+class base::redhat::epel inherits base::redhat {
+  package { "epel":
+    provider => "rpm",
+    source => "http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm",
+    ensure => "installed",
+  }
+}
