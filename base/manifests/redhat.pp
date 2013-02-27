494bb2e73a831e47229efa1f8968ba0e53882eb2
Oscar Renalias <oscar@renalias.net>
2013-02-27 23:49:02 +0200

    Install EPEL via exec rather than package, as it's actually easier...

--- base/manifests/redhat.pp
+++ base/manifests/redhat.pp
@@ -9,9 +9,9 @@ class base::redhat inherits base {
 # Enables the EPEL repository for RPM systems
 #
 class base::redhat::epel inherits base::redhat {
-  package { "epel":
-    provider => "rpm",
-    source => "http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm",
-    ensure => "installed",
+  # This was actually easier than doing it via the package type...
+  exec { "install-epel":
+    command => "/bin/rpm --replacepkgs http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm",
+    unless => "/usr/bin/test -f /etc/yum.repos.d/epel.repo"
   }
 }
