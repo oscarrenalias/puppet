60497f16eefad1863cb5ffb099ef89f13f40b57d
Oscar Renalias <oscar@renalias.net>
2013-03-14 23:43:08 +0200

    Create links to /usr/bin instead of /usr/local/bin, and create a link to npm too

--- nodejs/manifests/config.pp
+++ nodejs/manifests/config.pp
@@ -8,9 +8,16 @@ class nodejs::config inherits nodejs
   }
 
   # create a linl to the node binary
-  file { "/usr/local/bin/node":
+  file { "/usr/bin/node":
     ensure => "link",
     target => "${node_home}/bin/node",
+    require => Class["nodejs::install"],
+  }
+
+  # and to npm
+  file { "/usr/bin/npm":
+    ensure => "link",
+    target => "${node_home}/bin/npm",
     require => Class["nodejs::install"],
   }
 }
